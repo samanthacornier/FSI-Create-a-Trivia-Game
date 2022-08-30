3ef14994ac90406db3b36c911e861888b31dc588
samanthacornier <samantha112069@gmail.com>
2022-08-30 12:34:20 -0400

    bible trivia assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your Code Here
+let userName = window.prompt('samantha:')
+for(let i = 0; i < questions.length; i++){
+ let question = questions[i]   
+ let userAnswer = window.prompt (question.text)
+ if(userAnswer === question.correctAnswer) 
+ {userScore = userScore + 10}
+ {window.alert('Your score is: '+userScore)}
+ {console.log('correct')}
+    text: `What is the first book of the Old Testament?
+    A. Leviticus
+    B. Numbers
+    C. Genesis
+    D. Exodus`,
+    correctAnswer; "C"
+
+
+
+}
